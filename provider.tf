21d8e64c767ced5ece784086a84a5f43e1ebfc7e
Rakesh Karthikeyan <tk.rakesh@gmail.com>
2023-08-03 07:46:14 +0530

    first commit to TF cloud

--- provider.tf
+++ provider.tf
@@ -9,7 +9,8 @@ provider "google" {
   project = "terraform-gcp"
   region  = "us-central1"
   zone    = "us-central1-c"
-  credentials = file("c:\\terraform-cred\\key.json")
+  #credentials = file("c:\\terraform-cred\\key.json")
+  credentials = "${var.google_credentials}"
 }
 
 
